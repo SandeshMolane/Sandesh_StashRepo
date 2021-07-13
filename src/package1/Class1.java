b9f5ce574227f4c4973000b075e4671d7bc73a77
Sandesh Molane <sandeshmolane@yahoo.com>
2021-07-13 08:03:56 +0530

    First commit in branch A

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,7 +3,7 @@ package package1;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("First line Class1");
-		System.out.println("Second line Class1");
+		System.out.println("1 Branch A");
+		System.out.println("1.1 Branch A");
 	}
 }
